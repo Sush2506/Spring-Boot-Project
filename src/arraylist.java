5dc44137d16b0d4ac79a60eac62b160b58809fe5
Sush2506 <95991006+Sush2506@users.noreply.github.com>
2025-01-28 11:38:45 +0530

    Update arraylist.java

--- src/arraylist.java
+++ src/arraylist.java
@@ -3,7 +3,7 @@ public class arraylist {
 	public static void main(String args[])
 	{
 		ArrayList<Integer> list=new ArrayList<Integer>();
-		list.add(7);
+		list.add(8);
 		list.add(5);
 		list.add(3);
 		list.add(2);
